10cabaa5b29fb53562c05a89b5d47418aeb87c30
Rachel Johnson <Rachel.Johnson.2000@live.com>
2026-02-24 16:06:12 -0600

    static, classes, constructors, and inheritance

---
+++ Section1/Ebook.js
@@ -0,0 +1,20 @@
+class Book{
+  constructor(title, author, year)
+  {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+  }
+}
+
+class Ebook extends Book {
+    constructor(title, author, year, fileSize)
+    {
+       super(title, author, year); 
+       this.fileSize = fileSize;
+    }
+    download(title, fileSize)
+    {
+        return `$${this.title}} $${this.fileSize}}`
+    }
+}
---
+++ Section1/OnlineBank.js
@@ -0,0 +1,31 @@
+class BankAccount {
+    constructor(accountHolder, accountNumber,balance = 0) {
+        this.balance = balance;
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+    deposit(amt) {
+        if (amt > 0) {
+            this.balance += amt;
+            console.log(`Deposited $${amt}. New balance: $${this.balance}`);
+        } else {
+            console.log((`Invalid deposit amount: $${amt}. Deposit must be greater than 0.`));
+        }
+    }
+    withdraw(amt) {
+        if (amt < this.balance) {
+            this.balance -= amt; 
+            console.log(`Withdrew $${amt}. New balance: $${this.balance}`);
+        }
+        else {
+            console.log(`Insufficient funds. Current balance: $${this.balance}`);
+        }
+    }
+}
+
+class OnlineBankAccount extends BankAccount {
+    constructor(accountHolder, accountNumber, balance = 0) {
+        super(accountHolder, accountNumber, balance);
+         this.aaa = "test";
+    }
+}
